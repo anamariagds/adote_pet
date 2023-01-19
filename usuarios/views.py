108f654b465e64ad42ed07ef5730aef1c506618d
anamariagds <gomesabam@gmail.com>
2023-01-19 09:35:49 -0300

    Criando logout, usando a função logout do app auth do proprio Django.

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 
 def cadastro(request):
@@ -55,3 +55,6 @@ def logar(request):
         else:
             messages.add_message(request, constants.ERROR, 'Login ou senha invalido.')
             return render(request, 'login.html')
+def sair(request):
+    logout(request)
+    return redirect('/auth/login')
