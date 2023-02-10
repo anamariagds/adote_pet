5126e573e26f0824de402c61691ca43684a1f951
anamariagds <gomesabam@gmail.com>
2023-02-10 18:10:18 -0300

    Solicitação de adoção, processamento do pedido e dashboard

--- adotar/views.py
+++ adotar/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from divulgar.models import Pet, Raca
+from django.contrib.auth.decorators import login_required
+from django.contrib.messages import constants
+from django.contrib import messages
+from .models import PedidoAdocao
+from datetime import datetime
+from django.core.mail import send_mail
+
 
 def listar_pets(request):
     if request.method == "GET":
@@ -21,3 +28,50 @@ def listar_pets(request):
                                                     'racas': racas,
                                                     'cidade': cidade,
                                                     'raca_filter': raca_filter})
+
+@login_required
+def pedido_adocao(request, id_pet):
+   # if request.method =="POST":
+
+    pet = Pet.objects.filter(id=id_pet).filter(status="P")
+
+    if not pet.exists():
+        messages.add_message(request, constants.WARNING, 'Esse pet já foi adotado.')
+        return redirect('/adotar')
+
+    pedido = PedidoAdocao(pet=pet.first(),
+                          usuario = request.user,
+                          data=datetime.now())
+    pedido.save()
+    messages.add_message(request, constants.SUCCESS, 'Pedido feito com sucesso.')
+
+    print(pet)
+    return redirect('/adotar')
+
+#update no status do pedido, no banco de dados.
+@login_required
+def processa_pedido_adocao(request, id_pedido):
+    status = request.GET.get('status')
+    pedido = PedidoAdocao.objects.get(id=id_pedido)
+
+    if status == "A":
+        pedido.status = 'AP'
+        string = '''Olá, seu pedido de adoção foi aprovado!'''
+    elif status == "R":
+        pedido.status = 'R'
+        string = '''Olá, seu pedido de adoção foi recusado.'''
+    
+    pedido.save()
+
+    email = send_mail(
+        'Seu pedido de adoção foi processado',
+        string,
+        'anamaria@gmail.com',
+        [pedido.usuario.email,],
+    )
+
+    messages.add_message(request, constants.SUCCESS, 'Pedido de adoção processado com sucesso')
+    return redirect('/divulgar/ver_pedido_adocao')
+    
+    
+
--- divulgar/views.py
+++ divulgar/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages import constants
 from .models import Tag, Raca, Pet
+from adotar.models import PedidoAdocao
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required
 def novo_pet(request):
@@ -64,7 +66,39 @@ def remover_pet(request, id):
     messages.add_message(request, constants.SUCCESS, 'Removido com sucesso.')
     return redirect('/divulgar/seus_pets')
 
+def ver_pet(request, id):
+    if request.method == "GET":
+        pet = Pet.objects.get(id=id)
 
+        return render(request, 'ver_pet.html', {'pet': pet})
+
+@login_required
+def ver_pedido_adocao(request):
+    if request.method == "GET":
+        pedidos = PedidoAdocao.objects.filter(usuario=request.user).filter(status='AG')
+        return render(request, 'ver_pedido_adocao.html', {'pedidos': pedidos})
+
+@login_required
+def dashboard(request):
+    if request.method == "GET":
+        return render(request, 'dashboard.html')
+
+@csrf_exempt
+def api_adocoes_por_raca(request):
+    racas = Raca.objects.all()
+
+    qtd_adocoes = []
+    for raca in racas:
+        adocoes = PedidoAdocao.objects.filter(pet__raca=raca).filter(status='AP').count()
+        qtd_adocoes.append(adocoes)
+
+    racas = [raca.raca for raca in racas]
+    data = {'qtd_adocoes': qtd_adocoes,
+            'labels': racas}
+
+    return JsonResponse(data)
+   
+    
 
 
  
